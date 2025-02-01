4572f37a268acb51fade4f69000c284f3e230d2e
Terratadgh <kevin.mulholland@gmail.com>
2025-02-01 12:55:40 +0000

    Asteroid split on kill, small asteroids killed

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from constants import *
 from circleshape import CircleShape
 
@@ -13,3 +14,14 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += (self.velocity * dt)
         
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            new_angle = random.uniform(20,50)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid1.velocity=self.velocity.rotate(new_angle)*1.2
+            asteroid2.velocity=self.velocity.rotate(-new_angle)*1.2
